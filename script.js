f05f52235a9c7e5e7725dad45ca44a2c98ca7867
Kyle Ruane <108101096+ruanek@users.noreply.github.com>
2022-07-19 14:33:34 -0700

    changes

--- script.js
+++ script.js
@@ -3,5 +3,13 @@ import {handleFormSubmit} from "./helpers.js"
 // Listener for submit of form
 document.querySelector(".input_form").addEventListener("submit", handleFormSubmit)
 
+navigator.geolocation.getCurrentPosition(handlePositionSuccess, handlePositionError)
 
- 
+
+ function handlePositionSuccess(loc) {
+    console.log(loc.coords.latitude, loc.coords.longitude);
+ }
+
+ function handlePositionError() {
+    alert("WOW")
+ }
